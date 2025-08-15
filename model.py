dfbec8747a600ef8dccded0eb1901cb783e46780
tshjustin <j.tann2305@gmail.com>
2025-08-15 13:35:47 +0800

    Added self-attention

--- model.py
+++ model.py
@@ -114,11 +114,25 @@ def apply_rotary_embeddings(
     x_out = x_out.reshape(*x.shape)
     return x_out.type_as(x).to(device)
 
+def repeat_kv(x: torch.Tensor, n_rep: int): 
+    batch_size, seq_len, n_kv_heads, head_dim = x.shape
+
+    if n_rep == 1:
+        return x
+    
+    return (
+        # (B, Seq_Len, N_KV_Heads, 1, Head_Dim)
+        x[:, :, :, None, :]
+        # (B, Seq_Len, N_KV_Heads, N_Rep, Head_Dim)
+        .expand(batch_size, seq_len, n_kv_heads, n_rep, head_dim)
+        # (B, Seq_Len, N_KV_Heads * N_Rep, Head_Dim)
+        .reshape(batch_size, seq_len, n_kv_heads * n_rep, head_dim)
+    )
 
 class RMSNorm(nn.Module): 
     """
     Layer normalization 
-    
+
     proposed that recentering dont contribute much, but instead rescaling 
 
     hence find a statistic that is independent of the mean ==> RMS statistic instead 
@@ -137,6 +151,174 @@ class RMSNorm(nn.Module):
         # gamma * ai_hat
         return self.weight * self._norm(x.float().type_as(x))
 
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, args: ModelArgs): 
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim 
+        self.head_dim = args.dim // args.n_heads 
+
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        # normalize before the self attention 
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+        # normalizr before the FFN block 
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor): 
+        # (B, seq_len, dim) + (B, seq_len, dim) -> (B, seq_len, dim)
+        # apply attention on the normalized input embeddings 
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_complex)
+
+        # output after the skip connection 
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+
+        return out 
+    
+class SelfAttention(nn.Module):
+    """
+    1. split input embeddings to different heads 
+
+    2. apply the self-attention, where the Q interacts with K
+
+    3. self attention since all the Q K V comes from the same place --> not cross attention 
+
+    4. In the inferecne case --> still the same --> for the nth word, would use information from 1...n-1th word via self attention 
+
+    5. KV cache comes into play here so we dont have to calculate K, V again AND dont have to keep the previous tokens that were generated
+    
+    6. But MHA + KV-Cache --> alot of reads to the cache --> so reduce the amount of reads by lowering K V matrices 
+
+    7. So use GQA, N groups, k KV-groups that are shared amongst groups 
+    """ 
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        # Compared to the initlaly llama, this one dont have any parallization 
+
+        # KV - Heads 
+        self.n_kv_heads = self.n_heads if args.n_kv_heads is None else args.n_kv_heads
+
+        # Q Heads 
+        self.n_heads_q = self.n_heads 
+        
+        # The ratio of number of Q heads to KV-Heads --> how many times k,v repeated / query 
+        self.n_rep = self.n_heads_q // self.n_kv_heads
+
+        # part of the embeddings that are "visualized" by each head 
+        self.head_dim = args.dim // args.n_heads 
+
+        # K , Q , V matrics 
+        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False)
+        self.wv = nn.Linear(args.dim, args.n_kv_heads * self.head_dim, bias=False)
+        self.wo = nn.Linear(args.n_heads, args.n_heads * self.head_dim, bias=False)
+
+        # Cache --> one for key and one for values 
+        """
+        During Inference 
+
+        Given the input length / prompt 
+
+        1. prompt gets multiplied with K Q V matrics 
+
+        2. results in k q v vectors that are shareded to different heads 
+
+        3. since we are use GQA, K V < Q 
+
+        for each token in the input sequence:
+            they have a corresponding q k v vector 
+            
+            each k q v vector are then sharded to a smaller head / different view of the embedding 
+        
+        cache_k = [
+            # Batch 0 - batch_size=1, seq_len=4, n_kv_heads=2, head_dim=3
+            [
+                # Position 0: [[k_head0], [k_head1]]
+                [[1.2, 0.3, -0.1], [0.8, -0.2, 0.5]],
+
+                # Position 1: 
+                [[0.9, 0.1, 0.7], [0.3, 0.4, -0.3]],
+
+                # Position 2, 3...
+                [[...], [...]],
+                [[...], [...]]
+            ]
+        """
+        self.cache_k = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+        self.cache_v = torch.zeros((args.max_batch_size, args.max_seq_len, self.n_kv_heads, self.head_dim))
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        batch_size, seq_len, _ = x.shape() # (B, 1, dim)
+
+        # multiply with the query , key and value matrix 
+        # (B, 1, dim) --> (B, 1, HQ * head_dim)
+        xq = self.wq(x)
+
+        # (B, 1, dim) --> (B, 1, HKV * head_dim)
+        xk = self.wk(x)
+        xv = self.wv(x)
+
+        # (B, 1, HQ * head_dim) --> (B, 1, HQ * head_dim)
+        # split to different attention heads --> since self_n_kv_heads != n_heads_q, would be different number of resulting vectors
+        # [q1, q2, q3, ..., q512]  -> [q1, q2, ..., q64], [q65, q66, ..., q128]...
+        xq = xq.view(batch_size, seq_len, self.n_heads_q, self.head_dim)
+        xk = xk.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+        xv = xv.view(batch_size, seq_len, self.n_kv_heads, self.head_dim)
+
+        # now add the rotary embeddings --> dont affect the shape at all 
+        xq = apply_rotary_embeddings(xq, freqs_complex, device=x.device)
+        xk = apply_rotary_embeddings(xk, freqs_complex, device=x.device)
+
+        # KV - Cache 
+        # Replace the entry in the cache for this token 
+        self.cache_k[:batch_size, start_pos:start_pos+seq_len] = xk
+        self.cache_v[:batch_size, start_pos:start_pos+seq_len] = xv
+        
+        # Retrieve all the cached keys and values so far 
+        # (B, seq_len_kv, head_kv, head_dim)
+        """
+        Since number of heads of key and values != query heads 
+
+        We need to account for this in the calculation 
+        
+        1. Copy the single k/v head into multiple heads into a MHA architecture 
+        """
+        keys = self.cache_k[:batch_size, 0:start_pos+seq_len]
+        values = self.cache_v[:batch_size, 0:start_pos+seq_len]
+
+        # Repeat the heads of the K and V to reach the number of the queries 
+        keys = repeat_kv(keys, self.n_rep)
+        values = repeat_kv(values, self.n_rep)
+
+        # Now move the head dimension before sequence, since each head just reads a part of the embedding 
+        # (B, 1, H_Q, Head_Dim) -> (B, H_Q, 1, Head_Dim)
+        xq = xq.transpose(1, 2)
+
+        # (B, Seq_Len_KV, H_Q, Head_Dim) -> (B, H_Q, Seq_Len_KV, Head_Dim)
+        keys = keys.transpose(1, 2)
+
+        # (B, Seq_Len_KV, H_Q, Head_Dim) -> (B, H_Q, Seq_Len_KV, Head_Dim)
+        values = values.transpose(1, 2)
+
+        # (B, H_Q, 1, Head_Dim) @ (B, H_Q, Head_Dim, Seq_Len_KV) -> (B, H_Q, 1, Seq_Len_KV)
+        scores = torch.matmul(xq, keys.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+        # (B, H_Q, 1, Seq_Len_KV) -> (B, H_Q, 1, Seq_Len_KV)
+        scores = F.softmax(scores.float(), dim=-1).type_as(xq)
+
+        # (B, H_Q, 1, Seq_Len) @ (B, H_Q, Seq_Len_KV, Head_Dim) -> (B, H_Q, 1, Head_Dim)
+        output = torch.matmul(scores, values)
+
+        # (B, H_Q, 1, Head_Dim) -> (B, 1, H_Q, Head_Dim) -> (B, 1, Dim)
+        # Concatenation 
+        output = (output.transpose(1, 2).contiguous().view(batch_size, seq_len, -1))
+
+        return self.wo(output) # (B, 1, Dim) -> (B, 1, Dim)
     
 class Transformer(nn.Module): 
 
